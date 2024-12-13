1125734cb9710ee6160b0ff5244c3fccc638db44
minwn3318 <minwn3318@naver.com>
2024-12-13 22:32:18 +0900

    스크립터블오브젝트생성
    UI와 실제 인벤토리에 사용될 스크립터  오브젝트 설정
    - 실제 스탯과
    - 인벤토리 아이템 스탯
    이후 UI배치후 선택시 UI가 바뀌는 것 진행

---
+++ Assets/ScriptableObj/ScriptableObjScripts/GameStatus.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class GameStatus : ScriptableObject
+{
+    public string statusName;
+    public int volumeStauts;
+}
---
+++ Assets/ScriptableObj/ScriptableObjScripts/InventoryItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class InventoryItem : ScriptableObject
+{
+    public Texture2D createdObj;
+    public int count;
+}
